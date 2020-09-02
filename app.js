7a7c8db83b922b52a46268310ddaa338cb3dbabe
Alex <alex@syloper.com>
2020-09-02 15:13:23 -0300

    last changes in mobile

--- app.js
+++ app.js
@@ -1,90 +1,78 @@
-let texts = ['Typical Text Effect', 'That Impress', 'No One'];
+let texts = ["Typical Text Effect", "That Impress", "No One"];
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let currentText = "";
+let letter = "";
 
 (function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, index++);
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, index++);
 
-    document.querySelector('#text2').textContent = letter;
+  document.querySelector("#text2").textContent = letter;
 
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-    }
-    setTimeout(type, 200);
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 200);
 })();
 
 // MOBILE TYPE EFFECT
-let texts2 = ['Natbar Coming Soon'];
-let count2 = 0;
-let index2 = 0;
-let currentText2 = '';
-let letter2 = '';
+// let texts2 = ["Natbar Coming Soon"];
+// let count2 = 0;
+// let index2 = 0;
+// let currentText2 = "";
+// let letter2 = "";
 
-(function type() {
-    if (count2 === texts2.length) {
-        count2 = 0;
-    }
-    currentText2 = texts2[count2];
-    letter2 = currentText2.slice(0, index++);
+// (function type() {
+//   if (count2 === texts2.length) {
+//     count2 = 0;
+//   }
+//   currentText2 = texts2[count2];
+//   letter2 = currentText2.slice(0, index++);
 
-    document.querySelector('#text').textContent = letter2;
+//   document.querySelector("#text").textContent = letter2;
 
-    if (letter2.length === currentText2.length) {
-        count2++;
-        index = 0;
-    }
-    setTimeout(type, 500);
-})();
+//   if (letter2.length === currentText2.length) {
+//     count2++;
+//     index = 0;
+//   }
+//   setTimeout(type, 500);
+// })();
 
 
 // -------------LANGUAGES ------------
-function cambioTabs(event, imgs) {
-    // var tabSelectors = document.querySelector('.tab-selectors');
+const tabSelectors = document.querySelectorAll(".tab-selectors"); // the flags
+const tabContent = document.querySelectorAll(".tab-content"); // the texts
 
-    // var tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-contenido-item");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+tabSelectors.forEach((tab, tabIndex) => {
+  tab.addEventListener("click", (e) => {
+    e.preventDefault();
+    tabSelectors.forEach((tab) => {
+      tab.classList.remove("red-border");
+    });
+    tab.classList.add("red-border");
 
-    // para agarrar la clase (SEGUIR MIRANDO PARA QUE FUNCIONE):
-    // tablinks = document.getElementsByClassName("tablinks");
-    // for (i = 0; i < tablinks.length; i++) {
-    // tablinks[i].className = tablinks[i].className.replace("tab-border", "tab-border");
-    //     this.classList.add("tab-border");
-    //     const tabContentItem = document.querySelector(`#${this.id}-content`);
-    //     tabContentItem.classList.add("show");
-    // }
-
-    document.getElementById(imgs).style.display = "block";
-    // evt.currentTarget.className += "tab-border";
-}
-
-// NAVBAR OPTION BY SCROLLING 
-// testimonials
-window.scroll({
-    top: 1890,
-    left: 100,
-    behavior: 'smooth'
+    tabContent.forEach((content, contentIndex) => {
+      if (contentIndex == tabIndex) {
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
+      }
+    });
+  });
 });
 
-// scroll contact
-const scrollContact = document.querySelector('.scrollContact');
-scrollContact.addEventListener('click', (e) => {
-    e.preventDefault();
-    scrollTo(0, 2800);
-})
 
-const scrollAbout = document.querySelector('.scrollAbout');
+// NAVBAR
+// const nav = document.querySelector('.navMob');
 
-scrollAbout.addEventListener('click', (e) => {
-    e.preventDefault();
-    scrollTo(0, 900);
-})
+// window.onscroll = () => {
+//   if (document.documentElement.scrollTop >= 600) {
+//     nav.style.padding = "30px 0"
+//     nav.style.background = "red";
+//   }
+// }
